e0ec754ba334d845b93678c3ce6e8f7e0bdf8c97
Alejandro Sanchez <alex.9802@hotmail.com>
2021-06-02 19:14:41 -0500

    Fix vot

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,14 +7,35 @@ const User = require('../models/user.model')
 // Local Imports
 
 const createUser = async (req = request, res = response) => {
-    
-    const user = new User(req.body);
-    await user.save();
 
-    res.json({
-        ok: true,
-        body: req.body,
-    });
+    const {email} = req.body;
+    
+    try {   
+
+        const emailExist = await User.findOne({email: email})
+        if(emailExist){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Credentials are incorrect try again with valid credentials'
+            })
+        }
+
+        const user = new User(req.body);
+        await user.save();
+    
+        res.json({
+            ok: true,
+            user
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Check logs'
+        })
+    }
+
 }
 
 // Export Modules
