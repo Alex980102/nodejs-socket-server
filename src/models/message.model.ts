6bd73f318d45bd87074879fd200e8346d9fe3cf7
Alejandro Sanchez <alex.9802@hotmail.com>
2021-08-02 13:10:56 -0500

    Stage 10 Section 114 add messages sockets

---
+++ src/models/message.model.ts
@@ -0,0 +1,44 @@
+import { Document, Model, model, Schema } from 'mongoose';
+
+/**
+ * Interface to model the User Schema for TypeScript.
+ * @param name:string
+ * @param email:string
+ * @param password:string
+ * @param online:boolean
+ */
+export interface IMessage extends Document {
+    name: string
+    email: string;
+    password: string;
+    online: boolean;
+}
+
+const MessageSchema: any = new Schema({
+    from: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+
+}, {
+    timestamps: true
+});
+
+MessageSchema.method('toJSON', function(){
+    const {__v, _id, ...object} = this.toObject();
+    return object;
+});
+
+const Messages: Model<IMessage> = model("Message", MessageSchema);
+
+export default Messages;
